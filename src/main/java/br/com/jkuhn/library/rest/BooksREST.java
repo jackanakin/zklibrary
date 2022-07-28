75db9bae2fd365d13bc4c7370d0cc09dd56b142a
Jardel Kuhn <jardelkuhn@gmail.com>
2022-07-28 12:41:24 -0300

    #8 implementado modelmapper e convertido entidade para dto na endpoint REST

--- src/main/java/br/com/jkuhn/library/rest/BooksREST.java
+++ src/main/java/br/com/jkuhn/library/rest/BooksREST.java
@@ -1,7 +1,9 @@
 package br.com.jkuhn.library.rest;
 
+import br.com.jkuhn.library.dto.BookDTO;
 import br.com.jkuhn.library.entity.Book;
 import br.com.jkuhn.library.services.implementations.BookServiceImpl;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/rs/books")
@@ -18,19 +21,32 @@ public class BooksREST {
     @Autowired
     private BookServiceImpl bookServiceImpl;
 
+    @Autowired
+    private ModelMapper modelMapper;
+
     @GetMapping("")
-    public List<Book> get() {
-        return bookServiceImpl.getAllLocalBooks();
+    public ResponseEntity<List<BookDTO>> get() {
+        List<Book> bookList = bookServiceImpl.getAllLocalBooks();
+        List<BookDTO> bookDTOList = bookList.stream()
+                .map(this::convertToDto)
+                .collect(Collectors.toList());
+
+        return new ResponseEntity<>(bookDTOList, HttpStatus.OK);
     }
 
     @GetMapping("/{bookId}")
-    public ResponseEntity<Book> get(@PathVariable Long bookId) {
+    public ResponseEntity<BookDTO> get(@PathVariable Long bookId) {
         Book book = bookServiceImpl.getLocalBook(bookId);
 
         if (book == null){
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         } else {
-            return new ResponseEntity<>(book, HttpStatus.OK);
+            return new ResponseEntity<>(convertToDto(book), HttpStatus.OK);
         }
+    }
+
+    private BookDTO convertToDto(Book book) {
+        BookDTO bookDTO = modelMapper.map(book, BookDTO.class);
+        return bookDTO;
     }
 }
